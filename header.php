eed15804e0467c15db9505df9fc695834275c328
k_ozawa <kouta.ozawa@nadia.bz>
2025-11-19 19:50:37 +0900

    stepコンテンツコーディング

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width, viewport-fit=cover">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Shippori+Mincho:wght@400;500;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Shippori+Mincho:wght@400;500;700&display=swap" rel="stylesheet">
 <?php // ローダー表示時のスタイル記述場所 ?>
 
 <?php
