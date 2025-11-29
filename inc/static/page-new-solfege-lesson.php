e66374e4b0b488126d72f0125da71873a3368f34
k_ozawa <kouta.ozawa@nadia.bz>
2025-11-29 18:59:00 +0900

    新ソルフェージュ スタイル調整

--- inc/static/page-new-solfege-lesson.php
+++ inc/static/page-new-solfege-lesson.php
@@ -8,63 +8,145 @@
      </div>
      <div class="voice">
       <div class="voice-swiper">
-       <div class="swiper-wrapper">
-        <div class="voice-item swiper-slide">
-         <div class="-info">
-           <span class="-img">
-            <img src="https://placehold.jp/95x95.png" alt="" srcset="">
-           </span>
-           <div class="-grade">
-            <span>1期生</span>
-           </div>
-           <p class="-place">神奈川県 tuttiピアノ教室</p>
-           <p class="-name">福田早苗先生</p>
-         </div>
-         <!-- .-info -->
-          <div class="-body">
-            <p class="-title">楽譜の景色がみるみる変わっていくのは本当に衝撃的でした。</p>
-            <p class="-text">
-            「本物だ！」と直感で感じました。①確かな根拠と工夫された指導②音楽教育と子ども達への熱い想いと誠実で温かいお人柄これらに惹かれて、ベーシック、アドバンスと受講致しました。ベーシックでは根っことなる理論を学びつつ、様々な角度からスモールステップを組み立ててアプローチしていく方法は、どれも子どもの目線に立った「楽しく」をモットーとした実践的な指導法でした。アドバンスでは更にレベルアップした指導法に加え、私達指導者のこれからの未来を指導の方向性を考える貴重な機会と愛ある後押しをして下さいました。
-             正解が一つではない音楽に対して、どう楽譜を解釈して音楽表現していくか、時に迷う事は多々あります。それを裏付けるのがソルフェージュなのですが、みか先生の指導法では、子どもに「分かりやすい言葉」で、いかに子どもの「心が動く」やり方で育てていくかが示され、本当に衝撃的でした。音楽の鍵となる骨組みが見えるので、楽譜の景色がみるみる変わっていくのです。
-             またコーチングや沢山の研鑽を根底として、子どもは勿論のこと、私達指導者の個性も大切にしつつ未来に向けて心ある言葉をかけてパワー下さるお姿は本当に素敵な魔法使い…！！
-             学ぶことは人格も含めて誰に教わるかが重要！やはりみか先生の講座に飛び込んだ事は正解だと感じてます。
-             同じ講座生の先生方も本当に素敵な方々で、温かいチームと共に学び成長しあえた事は宝物の時間となりました。良縁で人生が変わる。みか先生とチームの先生方に出会えて本当に感謝でいっぱいです。
-            </p>
-          </div>
-        </div>
-        <div class="voice-item swiper-slide">
-         <div class="-info">
-           <span class="-img">
-            <img src="https://placehold.jp/95x95.png" alt="" srcset="">
-           </span>
-           <div class="-grade">
-            <span>1期生</span>
-           </div>
-           <p class="-place">神奈川県 tuttiピアノ教室</p>
-           <p class="-name">福田早苗先生</p>
-         </div>
-         <!-- .-info -->
-          <div class="-body">
-            <p class="-title">楽譜の景色がみるみる変わっていくのは本当に衝撃的でした。</p>
-            <p class="-text">
-            「本物だ！」と直感で感じました。①確かな根拠と工夫された指導②音楽教育と子ども達への熱い想いと誠実で温かいお人柄これらに惹かれて、ベーシック、アドバンスと受講致しました。ベーシックでは根っことなる理論を学びつつ、様々な角度からスモールステップを組み立ててアプローチしていく方法は、どれも子どもの目線に立った「楽しく」をモットーとした実践的な指導法でした。アドバンスでは更にレベルアップした指導法に加え、私達指導者のこれからの未来を指導の方向性を考える貴重な機会と愛ある後押しをして下さいました。
-             正解が一つではない音楽に対して、どう楽譜を解釈して音楽表現していくか、時に迷う事は多々あります。それを裏付けるのがソルフェージュなのですが、みか先生の指導法では、子どもに「分かりやすい言葉」で、いかに子どもの「心が動く」やり方で育てていくかが示され、本当に衝撃的でした。音楽の鍵となる骨組みが見えるので、楽譜の景色がみるみる変わっていくのです。
-             またコーチングや沢山の研鑽を根底として、子どもは勿論のこと、私達指導者の個性も大切にしつつ未来に向けて心ある言葉をかけてパワー下さるお姿は本当に素敵な魔法使い…！！
-             学ぶことは人格も含めて誰に教わるかが重要！やはりみか先生の講座に飛び込んだ事は正解だと感じてます。
-             同じ講座生の先生方も本当に素敵な方々で、温かいチームと共に学び成長しあえた事は宝物の時間となりました。良縁で人生が変わる。みか先生とチームの先生方に出会えて本当に感謝でいっぱいです。
-            </p>
-          </div>
-        </div>
-       </div>
-       <div class="swiper-button-prev" data-bg="<?= ASSET_URI . "/img/swiper-left.svg" ?>"></div>
-       <div class="swiper-button-next" data-bg="<?= ASSET_URI . "/img/swiper-right.svg" ?>"></div>
+        <div class="swiper-wrapper">
+          <div class="voice-item swiper-slide">
+            <div class="-info">
+              <span class="-img">
+                <img src="https://placehold.jp/95x95.png" alt="" srcset="">
+              </span>
+              <div class="-grade">
+                <span>1期生</span>
+              </div>
+              <p class="-place">神奈川県 tuttiピアノ教室</p>
+              <p class="-name">福田早苗先生</p>
+            </div>
+            <!-- .-info -->
+            <div class="-body">
+              <p class="-title">楽譜の景色がみるみる変わっていくのは本当に衝撃的でした。</p>
+              <p class="-text">
+              「本物だ！」と直感で感じました。①確かな根拠と工夫された指導②音楽教育と子ども達への熱い想いと誠実で温かいお人柄これらに惹かれて、ベーシック、アドバンスと受講致しました。ベーシックでは根っことなる理論を学びつつ、様々な角度からスモールステップを組み立ててアプローチしていく方法は、どれも子どもの目線に立った「楽しく」をモットーとした実践的な指導法でした。アドバンスでは更にレベルアップした指導法に加え、私達指導者のこれからの未来を指導の方向性を考える貴重な機会と愛ある後押しをして下さいました。
+              正解が一つではない音楽に対して、どう楽譜を解釈して音楽表現していくか、時に迷う事は多々あります。それを裏付けるのがソルフェージュなのですが、みか先生の指導法では、子どもに「分かりやすい言葉」で、いかに子どもの「心が動く」やり方で育てていくかが示され、本当に衝撃的でした。音楽の鍵となる骨組みが見えるので、楽譜の景色がみるみる変わっていくのです。
+              またコーチングや沢山の研鑽を根底として、子どもは勿論のこと、私達指導者の個性も大切にしつつ未来に向けて心ある言葉をかけてパワー下さるお姿は本当に素敵な魔法使い…！！
+              学ぶことは人格も含めて誰に教わるかが重要！やはりみか先生の講座に飛び込んだ事は正解だと感じてます。
+              同じ講座生の先生方も本当に素敵な方々で、温かいチームと共に学び成長しあえた事は宝物の時間となりました。良縁で人生が変わる。みか先生とチームの先生方に出会えて本当に感謝でいっぱいです。
+              </p>
+            </div>
+          </div><!-- .swiper-slide -->
+          <div class="voice-item swiper-slide">
+            <div class="-info">
+              <span class="-img">
+                <img src="https://placehold.jp/95x95.png" alt="" srcset="">
+              </span>
+              <div class="-grade">
+                <span>1期生</span>
+              </div>
+              <p class="-place">神奈川県 tuttiピアノ教室</p>
+              <p class="-name">福田早苗先生</p>
+            </div>
+            <!-- .-info -->
+            <div class="-body">
+              <p class="-title">楽譜の景色がみるみる変わっていくのは本当に衝撃的でした。</p>
+              <p class="-text">
+              「本物だ！」と直感で感じました。①確かな根拠と工夫された指導②音楽教育と子ども達への熱い想いと誠実で温かいお人柄これらに惹かれて、ベーシック、アドバンスと受講致しました。ベーシックでは根っことなる理論を学びつつ、様々な角度からスモールステップを組み立ててアプローチしていく方法は、どれも子どもの目線に立った「楽しく」をモットーとした実践的な指導法でした。アドバンスでは更にレベルアップした指導法に加え、私達指導者のこれからの未来を指導の方向性を考える貴重な機会と愛ある後押しをして下さいました。
+              正解が一つではない音楽に対して、どう楽譜を解釈して音楽表現していくか、時に迷う事は多々あります。それを裏付けるのがソルフェージュなのですが、みか先生の指導法では、子どもに「分かりやすい言葉」で、いかに子どもの「心が動く」やり方で育てていくかが示され、本当に衝撃的でした。音楽の鍵となる骨組みが見えるので、楽譜の景色がみるみる変わっていくのです。
+              またコーチングや沢山の研鑽を根底として、子どもは勿論のこと、私達指導者の個性も大切にしつつ未来に向けて心ある言葉をかけてパワー下さるお姿は本当に素敵な魔法使い…！！
+              学ぶことは人格も含めて誰に教わるかが重要！やはりみか先生の講座に飛び込んだ事は正解だと感じてます。
+              同じ講座生の先生方も本当に素敵な方々で、温かいチームと共に学び成長しあえた事は宝物の時間となりました。良縁で人生が変わる。みか先生とチームの先生方に出会えて本当に感謝でいっぱいです。
+              </p>
+            </div>
+          </div><!-- .swiper-slide -->
+
+        </div><!-- .swiper-wrapper -->
       </div><!-- .voice-swiper -->
-     </div>
+      <div class="swiper-button-prev" data-bg="<?= ASSET_URI . "/img/swiper-left.svg" ?>"></div>
+      <div class="swiper-button-next" data-bg="<?= ASSET_URI . "/img/swiper-right.svg" ?>"></div>
+     </div><!-- .voice -->
      <div class="c-pageSubTitle">
       <h3>COURSE</h3>
       <p>コース構成</p>
      </div>
+     <div class="course">
+      <div class="course-lead">
+        <h4><span>感性</span>を育て、<br class="is-sp"><span>教える力</span>を磨く。</h4>
+        <p>音楽を教えるということは、音の向こうにある“心”を育てること。<br>
+          Daisy Methodの３コースは、生徒の「できた！」の笑顔を導きながら、<br>
+          指導者自身の感性も育っていく、そんな学びのステップです。<br>
+          基礎から実践へ、そして創造へ。<br>
+          音楽の本質にふれる喜びを共に育てていきましょう。</p>
+      </div><!-- .course-lead -->
+      <div class="course-step">
+       <div class="-box -basic">
+          <p class="-type">BASIC</p>
+          <p class="-message">
+            <strong>レッスンの質と楽しさ<br class="is-sp">を<br class="is-pc">両立させる実践力</strong>
+          </p>
+          <div class="-img">
+            <img src="https://placehold.jp/220x220.png" alt="" srcset="">
+          </div>
+          <p class="-text">
+            導入期からソルフェージュの基礎をわかりやすく教える力を育てます。
+            うたハノン®を通して「読譜・リズム・音感」を統合的に指導できるようになり、レッスンの質と楽しさを両立させる実践力を身につけます。
+          </p>
+          <div class="-btn">
+            <a href="" class="c-btn -arrowRight">
+              <span>詳しくはこちら</span>
+            </a>
+          </div>
+        </div>
+        <div class="-box -advance">
+        <p class="-type">BASIC</p>
+          <p class="-message">
+            <strong>レッスンの質と楽しさ<br class="is-sp">を<br class="is-pc">両立させる実践力</strong>
+            <span class="-label">受講資格：Basic修了者</span>
 
+          </p>
+          <div class="-img">
+            <img src="https://placehold.jp/220x220.png" alt="" srcset="">
+          </div>
+          <p class="-text">
+            導入期からソルフェージュの基礎をわかりやすく教える力を育てます。
+            うたハノン®を通して「読譜・リズム・音感」を統合的に指導できるようになり、レッスンの質と楽しさを両立させる実践力を身につけます。
+          </p>
+          <div class="-btn">
+            <a href="" class="c-btn -arrowRight">
+              <span>詳しくはこちら</span>
+            </a>
+          </div>
+
+        </div>
+        <div class="-box -master">
+        <p class="-type">BASIC</p>
+          <p class="-message">
+            <strong>レッスンの質と楽しさ<br class="is-sp">を<br class="is-pc">両立させる実践力</strong>
+            <span class="-label">受講資格：Basic修了者</span>
+          </p>
+          <div class="-img">
+            <img src="https://placehold.jp/220x220.png" alt="" srcset="">
+          </div>
+          <p class="-text">
+            導入期からソルフェージュの基礎をわかりやすく教える力を育てます。
+            うたハノン®を通して「読譜・リズム・音感」を統合的に指導できるようになり、レッスンの質と楽しさを両立させる実践力を身につけます。
+          </p>
+          <div class="-btn">
+            <a href="" class="c-btn -arrowRight">
+              <span>詳しくはこちら</span>
+            </a>
+          </div>
+        </div>
+      </div><!-- .course-step -->
+     </div><!-- .course -->
+     <div class="other">
+      <p>
+      ソルフェージュを知ることは、先生自身の感性がもう一度、花ひらくこと。<br><br>
+      生徒を導くその手の中に、音楽の未来があります。<br>
+      教えるたびに、自分も育つ。その喜びを、共に奏でていきましょう。
+      </p>
+      <picture class="-img">
+        <source srcset="<?= ASSET_URI . "/img/course-piano_pc.svg" ?>" media="(min-width: 960px)">
+        <img src="<?= ASSET_URI . "/img/course-piano_sp.svg" ?>" alt="">
+      </picture>
+
+     </div>
     </div>
   </div>
 </div>
