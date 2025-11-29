ae9845765681e97250eedb07879d6b687c95b9d8
k_ozawa <kouta.ozawa@nadia.bz>
2025-11-29 23:22:47 +0900

    音楽教室

--- inc/static/page-piano-lesson.php
+++ inc/static/page-piano-lesson.php
@@ -2,7 +2,61 @@
 <div class="post_content mb-0">
   <div class="swell-block-fullWide alignfull pt-0 ">
     <div class="swell-block-fullWide__inner l-article">
-      テスト
+      <div class="piano-lesson__inner">
+        <div class="piano-lesson__lead">
+          <p class="-title">
+          <picture class="-img">
+            <img src="<?= ASSET_URI . "/img/piano-lesson__lead-img01.png" ?>" alt="強みを生かして、個性を伸ばす">
+          </picture>
+          </p>
+          <div class="-body">
+            <picture class="-img">
+              <source srcset="<?= ASSET_URI . "/img/piano-lesson__lead-img02_pc.png" ?>" media="(min-width: 960px)">
+              <img src="<?= ASSET_URI . "/img/piano-lesson__lead-img02_sp.png" ?>" alt="">
+            </picture>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="post_content mb-0">
+  <div class="swell-block-fullWide alignfull pt-0 ">
+    <div class="swell-block-fullWide__inner l-article">
+      <div class="piano-lesson__message">
+        <div class="piano-lesson__inner">
+          <h2 class="-title font-26-38">
+            <span>ピアノ</span>や<span>音楽</span>を通して<span><br class="is-sp">“できる”</span>自信を育み、<br>
+            <span>人間力</span>を育てる
+          </h2>
+          <p class="-text">
+          ピアノはコツコツ頑張れば必ず上達できます。<br>
+          そして弾けるようになったら自分も周りも幸せにできる習い事です。<br>
+          生徒一人ひとりが楽しく学びながら自己実現を積み重ね、ピアノが弾ける喜びを感じ、<br>
+          人生がより豊かになるよう 音楽でサポートします。
+          </p>
+          <div class="-item" data-bg="<?= ASSET_URI . "/img/icon_hana-yellow.svg" ?>">
+            <picture class="-img">
+              <source srcset="<?= ASSET_URI . "/img/piano-lesson/message01_pc.png" ?>" media="(min-width: 960px)">
+              <img src="<?= ASSET_URI . "/img/piano-lesson/message01_sp.png" ?>" alt="">
+            </picture>
+          </div>
+          <div class="-item" data-bg="<?= ASSET_URI . "/img/icon_hana-yellow.svg" ?>">
+            <picture class="-img">
+              <source srcset="<?= ASSET_URI . "/img/piano-lesson/message02_pc.png" ?>" media="(min-width: 960px)">
+              <img src="<?= ASSET_URI . "/img/piano-lesson/message02_sp.png" ?>" alt="">
+            </picture>
+          </div>
+          <div class="-item" data-bg="<?= ASSET_URI . "/img/icon_hana-yellow.svg" ?>">
+            <picture class="-img">
+              <source srcset="<?= ASSET_URI . "/img/piano-lesson/message03_pc.png" ?>" media="(min-width: 960px)">
+              <img src="<?= ASSET_URI . "/img/piano-lesson/message03_sp.png" ?>" alt="">
+            </picture>
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
 </div>
