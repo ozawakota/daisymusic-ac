7d49f3d3816f53210ebdcc8b6411032ea46ec42a
k_ozawa <kouta.ozawa@nadia.bz>
2025-11-30 20:01:40 +0900

    message cost

--- inc/static/page-piano-lesson.php
+++ inc/static/page-piano-lesson.php
@@ -54,9 +54,144 @@
               <img src="<?= ASSET_URI . "/img/piano-lesson/message03_sp.png" ?>" alt="">
             </picture>
           </div>
-
-        </div>
-      </div>
+          <!-- ボタン -->
+          <div class="-btn">
+            <a href="" class="c-btn -arrowRight -line -green">
+              <span>体験レッスンに申し込む</span>
+            </a>
+          </div>
+          <div class="-bnr">
+            <a href="">
+              <img src="<?= ASSET_URI . "/img/piano-lesson/message-bnr.png" ?>" alt="">
+            </a>
+          </div>
+        </div><!-- .piano-lesson__inner -->
+      </div><!-- .piano-lesson__message -->
     </div>
   </div>
 </div>
+
+
+<div class="post_content mb-0">
+  <div class="swell-block-fullWide alignfull " style="background:#FFF4E8;">
+    <div class="swell-block-fullWide__inner l-article">
+      <div class="piano-lesson__cost">
+        <div class="piano-lesson__inner">
+          <div class="c-pageSubTitle">
+            <h3>LESSON</h3>
+            <p>選べる<strong>4</strong>つのレッスン</p>
+          </div>
+          <div class="c-pageNote c-text-center mt-20">
+            <p>※レッスン価格は、2025年度現在の税別価格です。</p>
+          </div>
+          <!-- php -->
+          <div class="cost-step__wrap">
+            <div class="cost-step">
+              <div class="cost-step__fields">
+                <div class="cost-step__col">
+                  <span class="cost-step__item -num" data-bg="<?= ASSET_URI . "/img/icon_hana.png" ?>"></span>
+                  <div class="cost-step__item -title">
+                    <h3>スタンダードコース<br><span>(年間レッスン38回／月3.4回)</span></h3>
+                  </div>
+                  <div class="cost-step__item -text">
+                    <p>※発表会・ミニコンサート等レッスン回数に含みます。<br>
+                    ［追加単発レッスン］受験両立・コンサート対応等 30分/3,500円～ 対応します。</p>
+                  </div>
+                </div>
+                <div class="cost-step__col">
+                  <div class="cost-step__item -table">
+                  </div>
+                </div>
+              </div>
+              <div class="cost-step__btn">
+                <a href="" class="c-btn -arrowRight -line -green">
+                  <span>体験レッスンに申し込む</span>
+                </a>
+              </div>
+            </div>
+            <!-- 02 -->
+            <div class="cost-step">
+              <div class="cost-step__fields">
+                <div class="cost-step__col">
+                  <span class="cost-step__item -num" data-bg="<?= ASSET_URI . "/img/icon_hana.png" ?>"></span>
+                  <div class="cost-step__item -title">
+                    <h3>リトミック・<br>ソルフェージュコース</h3>
+                  </div>
+                  <!-- <div class="cost-step__item -text">
+                    <p>※発表会・ミニコンサート等レッスン回数に含みます。<br>
+                    ［追加単発レッスン］受験両立・コンサート対応等 30分/3,500円～ 対応します。</p>
+                  </div> -->
+                </div>
+                <div class="cost-step__col">
+                  <div class="cost-step__item -table">
+                  </div>
+                </div>
+              </div>
+              <div class="cost-step__btn">
+                <a href="" class="c-btn -arrowRight -line -green">
+                  <span>体験レッスンに申し込む</span>
+                </a>
+              </div>
+            </div>
+            <!-- 03 -->
+            <div class="cost-step">
+              <div class="cost-step__fields">
+                <div class="cost-step__col">
+                  <span class="cost-step__item -num" data-bg="<?= ASSET_URI . "/img/icon_hana.png" ?>"></span>
+                  <div class="cost-step__item -title">
+                    <h3>ソルフェージュレッスン<br>（単発）</h3>
+                  </div>
+                  <!-- <div class="cost-step__item -text">
+                    <p>※発表会・ミニコンサート等レッスン回数に含みます。<br>
+                    ［追加単発レッスン］受験両立・コンサート対応等 30分/3,500円～ 対応します。</p>
+                  </div> -->
+                </div>
+                <div class="cost-step__col">
+                  <div class="cost-step__item -table">
+                  </div>
+                </div>
+              </div>
+              <div class="cost-step__btn">
+                <a href="" class="c-btn -arrowRight -line -green">
+                  <span>体験レッスンに申し込む</span>
+                </a>
+              </div>
+            </div>
+            <!-- 04 -->
+            <div class="cost-step">
+              <div class="cost-step__fields">
+                <div class="cost-step__col">
+                  <span class="cost-step__item -num" data-bg="<?= ASSET_URI . "/img/icon_hana.png" ?>"></span>
+                  <div class="cost-step__item -title">
+                    <h3>保護者向け講座<br>【気質診断®<br>+コーチングセッション】</h3>
+                  </div>
+                  <div class="cost-step__item -text">
+                    <p>
+                      キッズコーチング®は認知発達心理学と気質心理学をベースにしたコーチングメソッドです。<br>
+                      生まれ持った「気質」を知ることで、我が子の「困った」が魅力に変わります。「育て方が悪かったのかも…」とお悩みの方、お子様の才能を引き出す言葉がけを学びたい方、ピアノの練習方法に限らず、母親としてご一緒に『子育て』を楽しみましょう♪<br>
+                      お時間の範囲内ならご家族皆さまの診断可能(家族のトリセツ)も可能です。<br>
+                      ご興味のある方はお気軽にご相談ください🎵
+                    </p>
+                  </div>
+                </div>
+                <div class="cost-step__col">
+                  <div class="cost-step__item -table">
+                  </div>
+                </div>
+              </div>
+              <div class="cost-step__btn">
+                <a href="" class="c-btn -arrowRight -line -green">
+                  <span>体験レッスンに申し込む</span>
+                </a>
+              </div>
+            </div>
+
+          </div><!-- .cost-step__wrap -->
+
+          <!-- php -->
+        </div><!-- .piano-lesson__inner -->
+      </div><!-- .piano-lesson__cost -->
+    </div>
+  </div>
+</div>
+
