6ba1f3bebbc1eaa02b7f4f76429bf68dd810a8be
k_ozawa <kouta.ozawa@nadia.bz>
2025-12-03 16:41:29 +0900

    ソルフェージュスクール

--- inc/static/page-solfege-school.php
+++ inc/static/page-solfege-school.php
@@ -1,7 +1,140 @@
 <div class="post_content mb-0">
-  <div class="swell-block-fullWide alignfull pt-0 solfege-seminar">
+  <div class="swell-block-fullWide alignfull pt-0">
     <div class="swell-block-fullWide__inner l-article">
-      seeee solfege-school
+      <div class="solfege-school">
+        <div class="solfege-school__inner">
+          <div class="solfege-school__lead">
+            <div class="-content">
+              <h2 class="-title">
+                <span class="-title-up">
+                  <em>音楽</em>がもっと<em>“わかる”</em><br class="is-sp">ようになる
+                </span>
+                <img src="<?= ASSET_URI . "/img/solfege-school/fukidashi.png" ?>" alt="" class="-fukidashi">
+                <span class="-title-down">
+                  <em>ソルフェージュ<br class="is-sp">レッスン</em>を
+                </span>
+              </h2>
+              <p class="-message">
+                楽譜が読める・音が聴ける・リズムが<br class="is-sp">感じられる。
+              </p>
+              <p class="-text">
+              ソルフェージュは、音楽を楽しむ力<br class="is-sp">そのものです。<br>Daisy Music Academyでは、<br class="is-sp">「感じてわかる」ことを大切に、<br>年齢や経験に合わせて“音楽の根っこ”を育てていきます。
+              </p>
+            </div>
+            <div class="-img">
+              <img src="<?= ASSET_URI . "/img/solfege-school/lead_pc.png" ?>" alt="">
+            </div>
+          </div><!-- solfege-school__lead -->
+        </div>
+      </div><!-- .solfege-school -->
+    </div>
+  </div>
+</div>
+
+<div class="post_content mb-0">
+  <div class="swell-block-fullWide alignfull pt-0">
+    <div class="swell-block-fullWide__inner l-article">
+      <div class="solfege-school">
+        <div class="solfege-school__inner">
+          <div class="solfege-school__about">
+            <div class="c-pageSubTitle">
+              <h3>ABOUT</h3>
+              <p>ソルフェージュとは？</p>
+            </div>
+            <p class="-message">
+              <span>
+                ソルフェージュとは、音楽のことばを<br class="is-sp">
+                理解するための学びです。
+              </span><br>
+              読譜・リズム・音感を身につけることで、<br>
+              演奏も、合唱も、アンサンブルもぐっと楽しくなります。
+            </p>
+            <picture class="-img">
+              <source srcset="<?= ASSET_URI . "/img/solfege-school/about-img_pc.png" ?>" media="(min-width: 960px)">
+              <img src="<?= ASSET_URI . "/img/solfege-school/about-img_sp.png" ?>" alt="">
+            </picture>
+            <div class="-dot">
+              <img src="<?= ASSET_URI . "/img/solfege-school/dot.svg" ?>" alt="">
+            </div>
+            <p class="-text">
+              そんな悩みの根っこを<br class="is-sp">
+              <span>やさしく解決</span>するのが<br>
+              <strong>ソルフェージュ</strong>です
+            </p>
+
+          </div><!-- solfege-school__about -->
+        </div>
+      </div><!-- .solfege-school -->
+    </div>
+  </div>
+</div>
+
+<div class="post_content mb-0">
+  <div class="swell-block-fullWide alignfull pt-0">
+    <div class="swell-block-fullWide__inner l-article">
+      <div class="solfege-school">
+        <div class="solfege-school__inner">
+          <div class="solfege-school__way">
+            <div class="c-pageSubTitle">
+              <h3>2<small>way</small></h3>
+              <p class="-type">選べる<span>2つ</span>の学び方</p>
+            </div>
+            <div class="-step-wrap">
+              <div class="-step">
+                <div class="-lead">
+                  <p class="-text">
+                    <span>全国どこからでも受講可能</span>
+                  </p>
+                  <p class="-title">オンラインで学ぶ</p>
+                </div>
+                <div class="-body">
+                  <div class="-img">
+                    <img src="<?= ASSET_URI . "/img/solfege-school/online-01.png" ?>" alt="">
+                  </div>
+                  <p class="-text">
+                    うたハノン®を中心に、「読譜・リズム・音感」を総合的に育てるオリジナルカリキュラム。
+                  </p>
+                </div>
+                <div class="-type">
+                  <p class="-fukidashi">
+                    <span>講師</span>
+                  </p>
+                  <div class="-person">
+                    <div class="-box">
+                      <img src="<?= ASSET_URI . "/img/solfege-school/online-person.png" ?>" alt="">
+                      <p class="-name">尾島 未佳</p>
+                      <a href="" class="-link">プロフィールはこちら</a>
+                    </div>
+                    <div class="-or">
+                      <p>OR</p>
+                    </div>
+                    <div class="-box">
+                      <img src="<?= ASSET_URI . "/img/solfege-school/online-menter.png" ?>" alt="">
+                      <p class="-name">
+                        Daisy Method<br>
+                        （Advance／Master）<br>
+                        修了の※認定講師
+                      </p>
+                      <a href="" class="-link">Daisy Methodの詳細はこちら</a>
+                    </div>
+                  </div><!-- .-person -->
+                  <div class="-explain">
+                    <strong>認定講師とは</strong>
+                    <p>Daisy Method の指導法を学び、各レベルを修了した講師です。</p>
+                    <ul>
+                      <li>Basic修了講師：導入期の基礎づくり</li>
+                      <li>Advance修了講師：発展・表現の指導</li>
+                      <li>Master修了講師：構造理解・教材づくり・レッスン設計</li>
+                    </ul>
+                    <p>どの講師も、「感じて教える」ことを大切に、<br>一人ひとりに寄り添ってレッスンを行います。</p>
+                  </div>
+                </div><!-- .-type -->
+              </div><!-- .-step -->
+
+            </div><!-- .-step-wrap -->
+          </div><!-- solfege-school__way -->
+        </div>
+      </div><!-- .solfege-school -->
     </div>
   </div>
 </div>
