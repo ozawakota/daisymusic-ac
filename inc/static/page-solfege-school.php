ab55d434d9cef6623aa143bd8f1de1c652723d4c
k_ozawa <kouta.ozawa@nadia.bz>
2025-12-04 21:00:07 +0900

    ソリュースタイル

--- inc/static/page-solfege-school.php
+++ inc/static/page-solfege-school.php
@@ -81,6 +81,7 @@
             </div>
             <div class="-step-wrap">
               <div class="-step">
+                <span class="-num" data-bg="<?= ASSET_URI . "/img/icon_hana.png" ?>"></span>
                 <div class="-lead">
                   <p class="-text">
                     <span>全国どこからでも受講可能</span>
@@ -121,14 +122,41 @@
                   <div class="-explain">
                     <strong>認定講師とは</strong>
                     <p>Daisy Method の指導法を学び、各レベルを修了した講師です。</p>
-                    <ul>
-                      <li>Basic修了講師：導入期の基礎づくり</li>
-                      <li>Advance修了講師：発展・表現の指導</li>
-                      <li>Master修了講師：構造理解・教材づくり・レッスン設計</li>
+                    <ul class="-hana">
+                      <li><span>Basic修了講師</span>：導入期の基礎づくり</li>
+                      <li><span>Advance修了講師</span>：発展・表現の指導</li>
+                      <li><span>Master修了講師</span>：構造理解・教材づくり・レッスン設計</li>
                     </ul>
-                    <p>どの講師も、「感じて教える」ことを大切に、<br>一人ひとりに寄り添ってレッスンを行います。</p>
+                    <p class="text-sp-center">どの講師も、「感じて教える」ことを大切に、<br>一人ひとりに寄り添ってレッスンを行います。</p>
                   </div>
                 </div><!-- .-type -->
+                <div class="-btn">
+                  <a href="" class="c-btn -arrowRight">
+                    <span>詳細を見る</span>
+                  </a>
+                </div>
+              </div><!-- .-step -->
+              <div class="-step">
+                <span class="-num" data-bg="<?= ASSET_URI . "/img/icon_hana.png" ?>"></span>
+                <div class="-lead">
+                  <p class="-text">
+                    <span>お住まいの地域で受講可能</span>
+                  </p>
+                  <p class="-title">対面で学ぶ</p>
+                </div>
+                <div class="-body">
+                  <div class="-img">
+                    <img src="<?= ASSET_URI . "/img/solfege-school/online-01.png" ?>" alt="">
+                  </div>
+                  <ul class="-check">
+                    <li>音を一緒に聴ける安心感</li>
+                    <li>体の動きでリズムを理解できる</li>
+                    <li>感覚的なやりとりが伝わりやすい</li>
+                  </ul>
+                  <p class="-text">
+                  対面ならではの魅力があります
+                  </p>
+                </div>
               </div><!-- .-step -->
 
             </div><!-- .-step-wrap -->
@@ -138,3 +166,52 @@
     </div>
   </div>
 </div>
+
+
+<div class="post_content mb-0">
+  <div class="swell-block-fullWide alignfull pt-0">
+    <div class="swell-block-fullWide__inner l-article">
+      <div class="solfege-school">
+        <div class="solfege-school__inner">
+          <div class="c-pageSubTitle">
+            <h3>RECOMMEND</h3>
+            <p>どんな人におすすめ？</p>
+          </div>
+          <div class="solfege-school__recommend">
+            <picture class="-img">
+              <source srcset="<?= ASSET_URI . "/img/solfege-school/recommend-img_pc.png" ?>" media="(min-width: 960px)">
+              <img src="<?= ASSET_URI . "/img/solfege-school/recommend-img_sp.png" ?>" alt="">
+            </picture>
+            <p class="-text">
+              <span>お子さま</span>から<span>大人</span>まで<br>どなたでも受講できます
+            </p>
+          </div><!-- .solfege-school__recommend -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="post_content mb-0">
+  <div class="swell-block-fullWide alignfull pt-0" >
+    <div class="swell-block-fullWide__inner l-article">
+      <div class="solfege-school -bg2" data-bg="<?= ASSET_URI . "/img/solfege-school/bg2.png" ?>">
+        <div class="solfege-school__inner">
+            <div class="solfege-school__other">
+                <p class="-message">
+                あなたの<span>音楽</span>を、<br class="is-sp">もっと<span>自由</span>に。<br>
+                一緒に楽しみながら<br class="is-sp">育てていきましょう。
+                </p>
+                <p class="-text">
+                ソルフェージュは、<br>
+ただ「読めるようになる」だけの学びではありません。<br>
+音楽の見え方が変わり、聴こえる世界が広がり、“わかる喜び”が心の中に灯っていきます。
+                </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
