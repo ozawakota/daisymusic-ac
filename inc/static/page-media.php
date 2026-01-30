44c7db40b102e72a399ff1af9f94815541a6efab
k_ozawa <kouta.ozawa@nadia.bz>
2026-01-30 19:36:47 +0900

    1/30 FB対応

--- inc/static/page-media.php
+++ inc/static/page-media.php
@@ -85,19 +85,48 @@
                 </div>
 
 
-                <div class="text_flex">
-                    <div class="text_box_style slash">
-                        <p class="main_font">
-                            雑誌・Web記事・インタビューなど、<br class="is-sp">掲載が決まりしだい順番に公開予定!<br>
-                            更新まで少しだけお待ちください。
-                        </p>
-                    </div>
-                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swell-block-fullWide alignfull pt-0">
+        <div class="swell-block-fullWide__inner l-article">
+            <div class="activity">
+                <img class="icon-radio is-sp" src="<?= ASSET_URI . "/img/media/icon-radio-sp.png" ?>" alt="">
 
-
-                <div class="text_box_style coming">
-                    <p class="comming_title">COMING<br class="is-sp"> SOON …</p>
+                <div class="activity__inner">
+                    <div class="-body">
+                        <div class="-box">
+                            <h4 class="-title">
+                            ラジオ<br class="is-sp">「松枝由紀子の<br>Sketch Book」に出演
+                            </h4>
+                            <div class="-img">
+                                <img src="<?= ASSET_URI . "/img/media/activity-img01.png" ?>" alt="">
+                            </div>
+                            <p class="-text">
+                            新大久保・高田馬場を拠点に長年、地域密着で <br class="is-pc">「松枝ぴあの教室」を運営されている代表・松枝由紀子さんがパーソナリティーを務めていらっしゃるラジオ番組に、このたびゲストとして出演させていただきました。
+                            </p>
+                            <a class="-link" href="https://open.spotify.com/episode/05NpvWysK9kS50LgbxbmDL?si=36fLoxMGThaYKHmXOJEd6A&nd=1&dlsi=41b1f37d74694ea8&fbclid=IwY2xjawPXOcZleHRuA2FlbQIxMABicmlkETE5RnhXQTFoN0ZKdDE0emh3c3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHjR1SK-_JXNVE-98uRD90nlCihC_5MPMdg3ykCpOklXFXHnF0o9Wxh25QSea_aem_3LrSmvGMy1prUfUx0tlTBw" target="_blank" rel="noopener noreferrer">
+                                <span>詳しくはこちら</span>
+                            </a>
+                        </div>
+                        <div class="-box">
+                            <h4 class="-title">
+                            「月刊ピアノ2026年2月号」<br>に掲載
+                            </h4>
+                            <div class="-img">
+                                <img src="<?= ASSET_URI . "/img/media/activity-img02.png" ?>" alt="">
+                            </div>
+                            <p class="-text">
+                            最新ヒットからスタンダードまで“弾きたかったあの曲”がきっと見つかるピアノマガジン「月刊ピアノ2026年2月号」に掲載いただきました。
+                            </p>
+                            <a class="-link" href="https://www.ymm.co.jp/p/detail.php?code=GTM01102809" target="_blank" rel="noopener noreferrer">
+                                <span>詳しくはこちら</span>
+                            </a>
+                        </div>
+                    </div><!-- .-body -->
                 </div>
+                <img class="icon-radio is-pc" src="<?= ASSET_URI . "/img/media/icon-radio.png" ?>" alt="">
+
             </div>
         </div>
     </div>
